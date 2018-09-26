fb37920905fb7609aebb9c8b42b7455e5cb79ca3
Ka Lung Sy <admin@hello2code.com>
2018-09-25 22:00:04 -0400

    adds react-to-print

--- src/Invoices.js
+++ src/Invoices.js
@@ -1,59 +1,72 @@
 import React from "react";
 import {Price} from './utilities/price.js';
 import DateFormat from './utilities/date-format.js';
+import ReactToPrint from "react-to-print";
 
-
-function Invoice(props){
-    let {invoice, user, cancel} = props;
-    return (
-        <div className={`servicebot-invoice-modal-container`}>
-            <div className={`__modal`}>
-                <div className={`__header`}>
-                    <div className={`__inner`}>
-                        <div className={`__left`}>
-                            <h3>{user.name || user.email}</h3>
-                            <span className={`__invoice-id`}>Invoice #: {invoice.invoice_id}</span>
-                            <span className={`__date`}><DateFormat date={invoice.date}/></span>
-                        </div>
-                        <div className={`__right`}>
+class Invoice extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        let {invoice, user, cancel} = this.props;
+        return (
+            <div className={`servicebot-invoice-modal-container`} ref={el => (this.componentRef = el)}>
+                <div className={`__modal`}>
+                    <div className={`__header`}>
+                        <div className={`__inner`}>
+                            <div className={`__left`}>
+                                <h3>{user.name || user.email}</h3>
+                                <span className={`__invoice-id`}>Invoice #: {invoice.invoice_id}</span>
+                                <span className={`__date`}><DateFormat date={invoice.date}/></span>
+                            </div>
+                            <div className={`__right`}>
                             <span role={`button`}
                                   aria-label={`close invoice modal`}
                                   className={`icon close`}
                                   onClick={cancel}/>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className={`__body`}>
-                    <h4 className={`__heading`}>Summary</h4>
-                    <div className={`mbf-summary`}>
-                        <p className={`_heading`}>Items</p>
-                        <div className={`_items`}>
-                        {invoice.references.user_invoice_lines.map((line, index) => {
-                            return (
-                                <p key={index} className={`_item`}>
-                                    <span className={`_label`}>{line.description}</span>
-                                    <span className={`_value_wrap`}>
+                    <div className={`__body`}>
+                        <h4 className={`__heading`}>Summary</h4>
+                        <div className={`mbf-summary`}>
+                            <p className={`_heading`}>Items</p>
+                            <div className={`_items`}>
+                                {invoice.references.user_invoice_lines.map((line, index) => {
+                                    return (
+                                        <p key={index} className={`_item`}>
+                                            <span className={`_label`}>{line.description}</span>
+                                            <span className={`_value_wrap`}>
                                         <span className={`_value`}>
                                             <Price value={line.amount} currency={line.currency}/>
                                         </span>
                                     </span>
-                                </p>
-                            );
-                        })}
+                                        </p>
+                                    );
+                                })}
+                            </div>
+                            <p className={`_total`}>
+                                <span className={`_label`}>Total: </span>
+                                <span className={`_value`}><Price value={invoice.total}
+                                                                  currency={invoice.currency}/></span>
+                            </p>
                         </div>
-                        <p className={`_total`}>
-                            <span className={`_label`}>Total: </span>
-                            <span className={`_value`}><Price value={invoice.total} currency={invoice.currency}/></span>
-                        </p>
+                    </div>
+                    <div className={`__footer`}>
+                        <ReactToPrint
+                            copyStyles={true}
+                            trigger={() => <button onClick={(e) => {
+                                window.print();
+                                return false;
+                            }} className={`buttons _primary _download-invoice`}>Print Invoice</button>}
+                            content={() => this.componentRef}
+                        />
                     </div>
                 </div>
-                <div className={`__footer`}>
-                    <button onClick={(e)=>{window.print();return false;} } className={`buttons _primary _download-invoice`}>Print Invoice</button>
-                </div>
+                <div onClick={cancel} className={`__backdrop`}/>
             </div>
-            <div onClick={cancel} className={`__backdrop`}/>
-        </div>
         );
+    }
 }
 class Invoices extends React.Component {
     constructor(props){
